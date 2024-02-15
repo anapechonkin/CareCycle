6ab66ab3bf6011860c652f0f01f9ebc9df526785
Ana Pechonkin <pechonkin.ana@gmail.com>
2024-02-15 13:30:41 -0500

    fixed bug with checkbox labels

--- carecycle/client/src/components/Checkbox.jsx
+++ carecycle/client/src/components/Checkbox.jsx
@@ -1,25 +1,25 @@
 const Checkbox = ({ title, options, onChange }) => {
-    return (
-      <div className="mb-4">
-        {title && <h3 className="font-semibold text-lg mb-2">{title}</h3>}
-        <div className="flex flex-wrap gap-2 justify-start items-center">
-          {options.map((option) => (
-            <label key={option.id} className="flex items-center cursor-pointer mr-2 mb-2 flex-nowrap" style={{ minWidth: '0', maxWidth: '100%' }}>
-              <input
-                type="checkbox"
-                id={option.id}
-                checked={option.checked}
-                onChange={() => onChange(option)}
-                disabled={option.disabled}
-                className="form-checkbox h-4 w-4 text-[#16839B] rounded border-gray-300 focus:ring-[#0f6a8b] mr-2"
-              />
-              <span className="text-md text-gray-700 flex-1 whitespace-nowrap overflow-hidden text-ellipsis" style={{ maxWidth: 'calc(100% - 1.5rem)' }}>{option.label}</span>
-            </label>
-          ))}
-        </div>
+  return (
+    <div className="mb-4">
+      {title && <h3 className="font-semibold text-lg mb-2">{title}</h3>}
+      <div className="flex flex-wrap gap-2 justify-start items-center">
+        {options.map((option) => (
+          <label key={option.id} className="flex items-center cursor-pointer mr-2 mb-2 flex-nowrap" style={{ minWidth: '0', maxWidth: '100%' }}>
+            <input
+              type="checkbox"
+              id={option.id}
+              checked={option.checked}
+              onChange={() => onChange(option)}
+              disabled={option.disabled}
+              className="form-checkbox h-4 w-4 text-[#16839B] rounded border-gray-300 focus:ring-[#0f6a8b] mr-2"
+            />
+            {/* Adjusted to use 'label' or 'name' */}
+            <span className="text-md text-gray-700 flex-1 whitespace-nowrap overflow-hidden text-ellipsis" style={{ maxWidth: 'calc(100% - 1.5rem)' }}>{option.label || option.name}</span>
+          </label>
+        ))}
       </div>
-    );
-  };
-  
-  
-  export default Checkbox;
+    </div>
+  );
+};
+
+export default Checkbox;
--- carecycle/client/src/pages/PageFourQuestionnaire.jsx
+++ carecycle/client/src/pages/PageFourQuestionnaire.jsx
@@ -45,7 +45,7 @@ const PageFourQuestionnaire = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <div className="flex justify-center mt-6 text-2xl">
-            <label className="flex items-center m">
+            <label className="flex items-center cursor-pointer">
               <input
                 type="radio"
                 name="rulesAccepted"
@@ -55,7 +55,7 @@ const PageFourQuestionnaire = () => {
               />
               <span className="ml-2">Accept</span>
             </label>
-            <label className="flex items-center mx-8">
+            <label className="flex items-center mx-8 cursor-pointer">
               <input
                 type="radio"
                 name="rulesAccepted"
