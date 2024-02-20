5524fd32167db73d6985c4b222fd5b9746227982
Ana Pechonkin <pechonkin.ana@gmail.com>
2024-02-20 12:55:50 -0500

    using .env for db config details for security, added .env to .gitignore

--- carecycle/server/config/db.js
+++ carecycle/server/config/db.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
+
+// Now using environment variables
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'carecycle',
-  password: 'Purell258', 
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
+
